53a73d8a926e43928350a6564c5fa1d696d77e57
gonzaggb <gonzaggb@hotmail.com>
2021-12-11 18:18:03 -0300

    listing tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-contract TodoList {
-    uint public taskCount = 0;
 
+contract TodoList {
+    uint256 public taskCount = 0;
+
+    //task structure
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    //array of tasks
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Checkout dappuniversitty.com");
+    }
+
+    //create a new task
+    function createTask(string memory _content) public {
+        taskCount++;
+        //put the new task in the array using the taskCount as the key
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
